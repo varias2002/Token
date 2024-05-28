3b9a4ab16d11653b9fbeddd720bcc390f67314f9
varias2002 <ariasvictor@3hotmail.com>
2024-05-28 11:19:31 -0500

    ingreso 28/05/2024

---
+++ auth0/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthModule } from '@auth0/auth0-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-nafqt6svh36f40e8.us.auth0.com',
+      clientId: 'lYNKnbnitoPemoX3Rx3hIP1DZSbyvAHQ',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
